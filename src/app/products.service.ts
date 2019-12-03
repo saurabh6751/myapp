a829f53c3b320f6d8df8328934d7e5006a1605b7
saurabh6751 <saurabhthombare6751@gmail.com>
2019-12-03 15:16:47 +0530

    initial app

---
+++ src/app/products.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  url = 'http://172.18.5.46:9898/products'
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get(this.url)
+  }
+}
